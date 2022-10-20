b5397e98661dfccf0ed205257fd77096d5ceeb5b
jansenosorio <dev.jansen93@gmail.com>
2022-10-20 19:27:19 -0300

    feat: inserido verificação do número, atendendo a politica de ser par e estar entre 4 e 14

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+// Variaveis globais
+let quantidadeCartas = 0
+
+// Array com todas as imagens
 const listaImagens = [
   'bobrossparrot',
   'desktop',
@@ -10,6 +14,28 @@ const listaImagens = [
   'unicornparrot'
 ]
 
+function entradaDosNumeros() {
+  quantidadeCartas = Number(prompt('Insira um número entre 4 e 14'))
+  verificarNumerosEntrada()
+}
+
+function verificarNumerosEntrada() {
+  let isImpar = quantidadeCartas % 2
+
+  if (isImpar == 1 || quantidadeCartas < 4) {
+    alert('O número precisa ser par e estar entre 4 e 14')
+    entradaDosNumeros()
+  } else {
+    alert('Bom jogo!')
+  }
+}
+
 function flip(item) {
   item.classList.toggle('flip')
 }
+
+function criarCartas() {
+  const cartas = document.querySelector('.container')
+}
+
+entradaDosNumeros()
