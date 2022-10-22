e5cbba59fa9916b305b408c8587d2e936212d98c
jansenosorio <dev.jansen93@gmail.com>
2022-10-21 21:54:44 -0300

    feat: inserido relógio contador

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 // Variaveis globais
 let quantidadeCartas = 0
+let contador = 0
+let contar = 0
 
 // Array com todas as imagens
 const listaImagens = [
@@ -18,6 +20,7 @@ let listImagensEmbaralhadaDuplicadas = []
 // Abertura da página, conferindo se as entradas do jogador estão de acordo com o solicitado
 
 function entradaDosNumeros() {
+  listaImagensEmbaralhada = []
   quantidadeCartas = Number(prompt('Insira um número entre 4 e 14'))
   verificarNumerosEntrada()
 }
@@ -52,32 +55,20 @@ function criarCartas() {
 
   embaralharCartas()
 
+  listImagensEmbaralhadaDuplicadas = [
+    ...listaImagensEmbaralhada,
+    ...listaImagensEmbaralhada
+  ].sort(function () {
+    return 0.5 - Math.random()
+  })
+
   for (let i = 0; i < quantidadeCartas; i++) {
-    listImagensEmbaralhadaDuplicadas = [
-      ...listaImagensEmbaralhada,
-      ...listaImagensEmbaralhada
-    ]
     contadorImagem = listImagensEmbaralhadaDuplicadas[i]
     cartas.innerHTML += `
           <div class="card" onclick="flip(this)" data-parrot="${contadorImagem}">
             <div class="face front" style="background-image: url('../img/${contadorImagem}.gif')"></div>
             <div class="face back"></div>
           </div>`
-  }
-}
-
-function encerrarJogo() {
-  const flipedCards = document.querySelectorAll('.flip')
-  const numeroCartas = document.querySelectorAll('.card')
-
-  if (flipedCards.length === numeroCartas.length) {
-    let continuarJogar = prompt('Você venceu o jogo, deseja continuar?')
-
-    if (continuarJogar === 'sim') {
-      return entradaDosNumeros()
-    } else if (continuarJogar === 'não') {
-      return alert('Obrigado por jogar!')
-    }
   }
 }
 
@@ -98,10 +89,12 @@ function flip(item) {
     item.classList.add('flip')
     segundoPersonagem = item.getAttribute('data-parrot')
 
-    setTimeout(verificarSeSaoIguais, 700)
+    setTimeout(verificarSeSaoIguais, 1000)
   }
 
-  setTimeout(encerrarJogo, 2500)
+  contador++
+
+  setTimeout(encerrarJogo, 500)
 }
 
 function verificarSeSaoIguais() {
@@ -116,4 +109,33 @@ function verificarSeSaoIguais() {
   }
 }
 
+function encerrarJogo() {
+  const flipedCards = document.querySelectorAll('.flip')
+  const numeroCartas = document.querySelectorAll('.card')
+  // let continuarJogar = ''
+
+  if (flipedCards.length == numeroCartas.length) {
+    alert(
+      `Você venceu o jogo em ${contador} tentativas e ${contar} segundos, parabéns!`
+    )
+    contar = ''
+    // console.log(continuarJogar)
+
+    // if (continuarJogar == 'sim') {
+    //   entradaDosNumeros()
+    // } else if (continuarJogar !== 'não') {
+    //   alert('Favor inserir "sim" ou "não"')
+    // } else {
+    //   alert('Obrigado por jogar!')
+    //   clearTimeout(encerrarJogo)
+    // }
+  }
+}
+
+function relogioContador() {
+  let segundos = document.querySelector('.relogio')
+  segundos.innerHTML = `${contar++}`
+}
+
 entradaDosNumeros()
+setInterval(relogioContador, 1000)
